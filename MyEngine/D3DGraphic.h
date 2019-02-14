2ec6a9c9d17086be5f195a92dec50b6c362f215f
LeJJ <36503113+Leavrth@users.noreply.github.com>
2019-02-14 17:25:18 +0800

    Add files via upload

--- MyEngine/D3DGraphic.h
+++ MyEngine/D3DGraphic.h
@@ -10,22 +10,21 @@ public:
 	D3DGraphic(HWND g_hWnd, int g_width, int g_height) :hWnd(g_hWnd), width(g_width), height(g_height) {};
 	LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
 
-	HRESULT D3D_Init(const float &, const float &);
+	HRESULT D3D_Init();
 	HRESULT D3D_Clean();
 	HRESULT D3D_infoDraw(float _mx, float _my, const float& _tmp);
 
-	HRESULT D3D_bgdDraw(float _cx, float _cy, const short (*mapId)[80]);
+	HRESULT D3D_bgdDraw(int _cx, int _cy, const short (*mapId)[80], const short);
 	HRESULT D3D_booDraw(float _mx, float _my);
 	~D3DGraphic();
 private:
 	HWND hWnd;
 	int width, height;
-	HRESULT D3D_ObjectInit(const float&, const float&);
+	HRESULT D3D_ObjectInit();
 	ID3DXFont *g_pFont;
 
+	RECT rect[3];
 	LPDIRECT3DTEXTURE9 g_pTexMap[3];
-	LPDIRECT3DTEXTURE9 g_pTexMan;
-	LPDIRECT3DTEXTURE9 g_pTexBGD;
 	LPDIRECT3DTEXTURE9 g_pTexBoo;
 	LPD3DXSPRITE       g_pSpr;
 };
